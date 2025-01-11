c3d7d5cb0e5daeebd8c4d306b56840f62791c103
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-12 01:26:26 +0800

    feat(Header): integrate tailwind-merge and update logo handling
    - Installed tailwind-merge for handling dynamic and conflicting Tailwind classes
    - Updated Header component to use twMerge for logo class merging
    - Removed width and height attributes from logo.svg, allowing size to be controlled via CSS

---
+++ 06-single-page-design-portfolio/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { twMerge } from "tw-merge";
+import Logo from "src/assets/logo.svg?react";
+
+export function Header() {
+  return (
+    <header className="flex justify-between items-center mb-8 outline outline-red-100">
+      <a href="#">
+        <Logo className={twMerge(`h-12 w-12`)} />
+      </a>
+      <button className="bg-black w-44 h-11 text-white rounded-full">
+        Free Consultation
+      </button>
+    </header>
+  );
+}
