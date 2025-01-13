f1d4a7162459083afd2c0fdf2b306cff8c7c7b5a
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-13 19:36:00 +0800

    feat(styles): enhance Tailwind config and setup font styling
    - Added custom colors and screen size in tailwind.config.js.
    - Imported Plus Jakarta Sans font and applied it to body in index.css.
    - Configured Prettier with Tailwind CSS plugin for better formatting.

--- 06-single-page-design-portfolio/tailwind.config.js
+++ 06-single-page-design-portfolio/tailwind.config.js
@@ -8,6 +8,17 @@ export default {
     extend: {
       colors: {
         galacticBlue: "#755CDE",
+        black: "#030303",
+        gray: "#7A736E",
+        lightCream:"#FFF7F0",
+        summerYellow: "#F6A560",
+        pink:"#F39E9E",
+        lightRed: "#EB7565",
+        cyan:"#61C4B7",
+        purple: "#552049"
+      },
+      screens: {
+        '1440': '1440px',
       }
     },
   },
