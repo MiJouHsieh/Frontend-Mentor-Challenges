6e36f18a38f4fcbb88fbcc9487ac0a8ca7a4ad64
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-15 23:15:04 +0800

    feat(App): add AboutSection component and adjust layout styles
    - Imported and integrated AboutSection component into the main App layout
    - Updated flex container styles to include px-0 for larger screens

--- 06-single-page-design-portfolio/src/App.jsx
+++ 06-single-page-design-portfolio/src/App.jsx
@@ -1,13 +1,15 @@
 import { Header } from "src/components/Header";
 import { HeroSection } from "src/components/HeroSection";
 import { ServiceGrid } from "src/components/ServiceGrid";
+import { AboutSection } from "src/components/AboutSection";
 
 function App() {
   return (
-    <div className="mx-auto flex min-w-[375px] flex-col items-center p-4 md:px-[39px] md:pt-[34px] 1440:max-w-[1110px]">
+    <div className="mx-auto flex min-w-[375px] flex-col items-center p-4 md:px-[39px] md:pt-[34px] 1440:max-w-[1110px] 1440:px-0">
       <Header />
       <HeroSection />
       <ServiceGrid />
+      <AboutSection />
     </div>
   );
 }
