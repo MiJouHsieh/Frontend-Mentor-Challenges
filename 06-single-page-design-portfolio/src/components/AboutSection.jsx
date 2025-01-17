511d0ca6ea652d543c31951d3d0a3b13fa8b8ef4
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-17 16:55:24 +0800

    style(AboutSection): add margin-bottom for improved spacing
    - Added mb-[100px] to the container for better vertical spacing

--- 06-single-page-design-portfolio/src/components/AboutSection.jsx
+++ 06-single-page-design-portfolio/src/components/AboutSection.jsx
@@ -1,10 +1,10 @@
 export function AboutSection() {
   return (
-    <div className="flex w-full flex-col items-center gap-10 outline outline-1 md:w-[111.88%] md:max-w-[1300px] md:flex-row md:justify-between md:gap-[69px] 1440:w-full 1440:gap-[125px]">
+    <div className="mb-[100px] flex w-full flex-col items-center gap-10 outline outline-1 md:mb-[120px] md:w-[111.88%] md:max-w-[1300px] md:flex-row md:justify-between md:gap-[69px] 1440:w-full 1440:gap-[125px] 1440:mb-[88px]">
       <div className="w-[88%] md:w-[47%] md:max-w-[445px] 1440:w-[40%]">
         <img src="src/assets/image-amy.webp" alt="" />
       </div>
-      <div className="flex w-full flex-col gap-6 text-center md:w-[44%] 1440:w-[48%]">
+      <div className="flex w-full flex-col items-center gap-6 text-center md:w-[44%] 1440:w-[48%]">
         <h1 className="title-xs md:title-s 1440:title-m">
           I’m Amy, and I’d love to work on your next project
         </h1>
