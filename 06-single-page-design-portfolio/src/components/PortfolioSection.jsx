ea1c446d4c02ac79ad5a25b6a60308c27e1dbd99
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-17 21:34:32 +0800

    feat(PortfolioSection): add navigation functionality for portfolio items
    Added navigation functionality to the PortfolioSection component:
    - Implemented `handleClickPrevious` and `handleClickNext` to navigate through portfolio items.
    - Used `useState` to manage the current index of displayed items.
    - Updated buttons to trigger navigation logic for improved interactivity.
    
    This change allows users to browse portfolio items interactively using navigation buttons.

--- 06-single-page-design-portfolio/src/components/PortfolioSection.jsx
+++ 06-single-page-design-portfolio/src/components/PortfolioSection.jsx
@@ -1,8 +1,23 @@
 import ArrowLeft from "src/assets/icon-arrow-left.svg?react";
 import ArrowRight from "src/assets/icon-arrow-right.svg?react";
 import { PORTFOLIO_LIST } from "src/constant";
+import { useState } from "react";
 
 export function PortfolioSection() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClickPrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? PORTFOLIO_LIST.length - 1 : prevIndex - 1,
+    );
+  };
+
+  const handleClickNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === PORTFOLIO_LIST.length - 1 ? 0 : prevIndex + 1,
+    );
+  };
+
   return (
     <section className="mb-[100px] flex w-[calc(100%+32px)] flex-col items-center gap-8 overflow-hidden outline outline-teal-100 md:-mx-8 md:mb-[120px] md:w-[calc(100%+78px)] md:gap-14 1440:mb-[88px]">
       <h1 className="text-2xl font-bold">My Work</h1>
@@ -23,10 +38,17 @@ export function PortfolioSection() {
         })}
       </div>
       <div className="flex gap-4">
-        <button className="flex h-16 w-16 items-center justify-center rounded-full bg-black hover:bg-galacticBlue">
+        <button
+          onClick={handleClickPrevious}
+          className="flex h-16 w-16 items-center justify-center rounded-full bg-black hover:bg-galacticBlue"
+        >
           <ArrowLeft />
         </button>
-        <button className="flex h-16 w-16 items-center justify-center rounded-full bg-black hover:bg-galacticBlue">
+
+        <button
+          onClick={handleClickNext}
+          className="flex h-16 w-16 items-center justify-center rounded-full bg-black hover:bg-galacticBlue"
+        >
           <ArrowRight />
         </button>
       </div>
