7a4be7ae3a551d3cd407c35359e8ac4f4cf2a9eb
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-19 13:52:31 +0800

    feat(portfolio): enhance carousel logic and responsive styles
    - Updated carousel `translateX` logic for mobile and tablet widths.
    - Ensured PortfolioSection aligns with parent container using dynamic widths.
    - Added gap adjustments between items for consistent spacing.
    - Applied overflow-hidden to hide content outside the visible viewport.

--- 06-single-page-design-portfolio/src/components/PortfolioSection.jsx
+++ 06-single-page-design-portfolio/src/components/PortfolioSection.jsx
@@ -5,39 +5,47 @@ import { useState } from "react";
 
 export function PortfolioSection() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const TOTAL_IMAGES = PORTFOLIO_LIST.length;
 
   const handleClickPrevious = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? PORTFOLIO_LIST.length - 1 : prevIndex - 1,
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + TOTAL_IMAGES) % TOTAL_IMAGES,
     );
   };
 
   const handleClickNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === PORTFOLIO_LIST.length - 1 ? 0 : prevIndex + 1,
-    );
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % TOTAL_IMAGES);
   };
-
+  const baseTranslateX = currentIndex * 280; // for mobile (270 + 10)
+  const mdTranslateX = currentIndex * 500; // for tablet (540 + 20)
   return (
-    <section className="mb-[100px] flex w-[calc(100%+32px)] flex-col items-center gap-8 overflow-hidden outline outline-teal-100 md:-mx-8 md:mb-[120px] md:w-[calc(100%+78px)] md:gap-14 1440:mb-[88px]">
-      <h1 className="text-2xl font-bold">My Work</h1>
-
-      <div className="flex w-[1410px] gap-[15px] overflow-hidden md:w-[2820px] md:gap-[30px]">
-        {PORTFOLIO_LIST.map((item) => {
-          return (
+    <section className="absolute left-1/2 mb-[100px] flex h-[338px] w-[calc(100%+32px)] -translate-x-1/2 flex-col items-start gap-8 overflow-hidden md:h-[576px] md:w-[calc(100%+78px)] 1440:w-[1440px]">
+      <h1 className="w-full overflow-hidden text-center text-2xl font-bold">
+        My Work
+      </h1>
+      <div className="flex h-[180px] w-[1410px] justify-center overflow-hidden md:h-[360px] md:w-[2820px]">
+        <div
+          className={`md:w-width-tablet w-width-mobile flex justify-between gap-[15px] overflow-hidden transition-transform duration-300 md:gap-[30px]`}
+          style={{
+            // transform: `translateX(-${currentIndex * (270 + 10)}px)`,
+            transform: `translateX(-${window.innerWidth >= 768 ? mdTranslateX : baseTranslateX}px)`,
+          }}
+        >
+          {PORTFOLIO_LIST.map((item, index) => (
             <div
-              key={item.id}
-              className="h-full w-[72%] object-contain md:w-[70%]"
+              key={index}
+              className={`w-[270px] md:w-[540px] ${index === 0 && "ml-9 md:ml-[300px]"}`}
             >
               <img
                 src={item.src}
                 alt={`Portfolio item ${item.id}`}
+                className="h-full w-full object-cover"
               />
             </div>
-          );
-        })}
+          ))}
+        </div>
       </div>
-      <div className="flex gap-4">
+      <div className="flex w-full justify-center gap-4">
         <button
           onClick={handleClickPrevious}
           className="flex h-16 w-16 items-center justify-center rounded-full bg-black hover:bg-galacticBlue"
