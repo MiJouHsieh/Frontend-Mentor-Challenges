e03a3327904c5f61605face1d67be88b8628f604
MiJouHsieh <mijouhsieh@gmail.com>
2025-02-01 02:00:39 +0800

    style(App): adjust position for PatternRings

--- 07-single-page-developer-portfolio/src/App.jsx
+++ 07-single-page-developer-portfolio/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className="relative flex min-w-[375px] flex-col items-center justify-center bg-black text-white">
       <PatternRings className="absolute left-[-342px] top-[124px] z-10 h-[129px] md:left-[-265px] md:top-[86px] 1440:left-[-100px] 1440:top-[133px]" />
       <PatternCircle className="absolute left-[311px] top-[254px] z-10 h-[129px] md:z-30 md:left-[702px] md:top-[471px] 1440:left-[766px] 1440:top-[521px]" />
-      <PatternRings className="absolute left-[189px] top-[1287px] z-10 h-[129px] md:left-[570px] md:top-[1047px] 1440:left-[-100px] 1440:top-[133px]" />
+      <PatternRings className="absolute left-[189px] top-[1287px] z-10 h-[129px] md:left-[570px] md:top-[1047px] 1440:left-[1491px] 1440:top-[1077px]" />
       <PatternRings className="absolute left-[-343px] top-[4615px] h-[129px] md:left-[-368px] md:top-[3304px] 1440:left-[-205px] 1440:top-[3524px]" />
       <div className="relative flex w-full min-w-[375px] flex-col items-center px-4 md:px-8 1440:max-w-[1110px] 1440:p-0">
         <Header />
