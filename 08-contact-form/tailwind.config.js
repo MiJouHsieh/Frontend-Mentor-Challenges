51e4710774d9356415f65edfd18d64a068c2d36c
MiJouHsieh <mijouhsieh@gmail.com>
2025-02-19 00:12:57 +0800

    chore(styles): extend Tailwind CSS theme with custom colors
    - Added custom colors (`grey900`, `grey500`, `green600`, `green200`, `red`) to Tailwind CSS theme
    - Updated `index.css` with `#E0F1E8` as background color

--- 08-contact-form/tailwind.config.js
+++ 08-contact-form/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey900: "#2A4144",
+        grey500: "#86A2A5",
+        green600: "#0C7D69",
+        green200: "#E0F1E8",
+        red: "#D73C3C"
+      }
+    },
   },
   plugins: [],
 }
