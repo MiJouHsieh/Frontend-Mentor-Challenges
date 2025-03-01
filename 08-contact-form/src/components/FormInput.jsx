7e98a72f2d046b22134f66f59b65c8f05b47d8ec
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-02 00:08:50 +0800

    feat(FormInput): integrate Formik useField for form handling
    - Added Formik's `useField` hook to manage form state
    - Updated label `htmlFor` to use `props.id || props.name` for better accessibility
    - Applied dynamic error styling to input fields based on Formik's meta state
    
    This update improves form handling, validation, and styling consistency.

--- 08-contact-form/src/components/FormInput.jsx
+++ 08-contact-form/src/components/FormInput.jsx
@@ -1,11 +1,18 @@
-export function FormInput({label}) {
+import {  useField } from "formik";
+
+export function FormInput({ label, ...props }) {
+  const [field, meta] = useField(props)
   return (
     <div className="flex flex-1 flex-col space-y-2">
-      <label htmlFor="">{label}</label>
+      <label htmlFor={props.id || props.name}>{label}</label>
       <input
-        type="text"
-        className="body-m h-[51px] rounded-lg border border-grey500 px-6 py-3"
+        {...field}
+        {...props}
+        className={`text-input body-m ${meta.touched && meta.error ? 'border-red':"" } h-[51px] rounded-lg border border-grey500 px-6 py-3`}
       />
+      {meta.touched && meta.error ? (
+        <span className="error text-red">{meta.error}</span>
+      ) : null}
     </div>
   );
 }
