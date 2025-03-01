682d5899f7f9389d9552b4de4a36ca3977390037
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-02 00:16:25 +0800

    feat(FormPage): integrate Formik and Yup for form handling
    - Replaced static form structure with Formik for state management
    - Added Yup validation schema for first name, last name, and email fields
    - Implemented form submission handling with a simulated delay
    - Updated input fields to use Formik's `name` and validation logic

--- 08-contact-form/src/components/FormPage.jsx
+++ 08-contact-form/src/components/FormPage.jsx
@@ -3,22 +3,67 @@ import { QueryTypeSelect } from "src/components/QueryTypeSelect";
 import { FormTextarea } from "src/components/FormTextarea";
 import { Checkbox } from "src/components/Checkbox";
 import { SubmitButton } from "src/components/SubmitButton";
+import { Formik, Form } from "formik";
+import * as Yup from 'yup'
 
 export function FormPage() {
   return (
-    <div className="w-full min-w-[343px] rounded-2xl bg-white p-6 text-grey900 outline md:w-[690px] md:p-10 1440:w-[736px]">
-      <h1 className="title mb-8">Contact Us</h1>
-      <div className="flex flex-col space-y-6">
-        <div className="space-y-6 md:flex md:justify-between md:gap-x-4 md:space-y-0">
-          <FormInput label="First Name" />
-          <FormInput label="Last Name" />
-        </div>
-        <FormInput label="Email Address" />
-        <QueryTypeSelect />
-        <FormTextarea />
-      </div>
-      <Checkbox />
-      <SubmitButton />
-    </div>
+    <>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("This field is required"),
+          lastName: Yup.string()
+            .max(20, "Must be 20 characters or less")
+            .required("This field is required"),
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Please enter a valid email address"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form
+          className="w-full min-w-[343px] rounded-2xl bg-white p-6 text-grey900 outline md:w-[690px] md:p-10 1440:w-[736px]"
+        >
+          <h1 className="title mb-8">Contact Us</h1>
+          <div className="flex flex-col space-y-6">
+            <div className="space-y-6 md:flex md:justify-between md:gap-x-4 md:space-y-0">
+              <FormInput
+                label="First Name"
+                name="firstName"
+                type="text"
+              />
+
+              <FormInput
+                label="Last Name"
+                name="lastName"
+                type="text"
+              />
+            </div>
+            <FormInput
+              label="Email Address"
+              name="email"
+              type="email"
+            />
+
+            <QueryTypeSelect />
+            <FormTextarea />
+          </div>
+          <Checkbox />
+          <SubmitButton />
+        </Form>
+      </Formik>
+    </>
   );
 }
