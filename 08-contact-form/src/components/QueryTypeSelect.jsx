85855d7bb408848c21b336bb117467ffeb81a4e4
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-03 15:01:22 +0800

    feat(QueryTypeSelect): add state management for radio selection
    - Introduced `useState` to manage selected radio option
    - Updated input type to `radio` for better UX and accessibility
    - Applied dynamic styling based on selected state
    
    This update enhances the usability of the query type selection component.
    
    style(assets): update icon-success-check.svg for consistency
    
    - Adjusted icon height to `20px` to match design consistency
    - Ensured proper SVG formatting
    
    style(css): refine checkbox and radio button styles
    
    - Adjusted `checkboxIconStyle` margin from `m-1.5` to `m-[3px]` for better spacing
    - Added `radioIconStyle` with improved appearance and checked state styling

--- 08-contact-form/src/components/QueryTypeSelect.jsx
+++ 08-contact-form/src/components/QueryTypeSelect.jsx
@@ -1,28 +1,47 @@
-// import IconRadioSelected from "src/assets/icon-radio-selected.svg?react";
+import IconRadioSelected from "src/assets/icon-radio-selected.svg?react";
+import { useState } from "react";
 
 export function QueryTypeSelect() {
+  const [radioChecked, setRadioChecked] = useState("");
+
   return (
     <div className="flex flex-col space-y-4">
       <label>Query Type</label>
       <div className="space-y-4 md:flex md:justify-between md:gap-x-4 md:space-y-0">
-        <div className="h-[51px] space-x-3 rounded-lg border border-grey500 px-4 py-3 md:flex-1">
+        <div
+          className={`${
+            radioChecked === "general-enquiry"
+              ? "border-green600 bg-green200"
+              : "border-grey500"
+          } relative flex h-[51px] items-center space-x-3 rounded-lg border px-6 py-3 md:flex-1`}
+        >
           <input
-            type="checkbox"
+            checked={radioChecked === "general-enquiry"}
+            type="radio"
             id="general-enquiry"
-            name="general-enquiry"
+            name="query-type"
+            value="general-enquiry"
+            className="radioIconStyle peer"
+            onChange={() => setRadioChecked("general-enquiry")}
           />
-          {/* <IconRadioSelected /> */}
+          <IconRadioSelected className="pointer-events-none absolute left-3 hidden peer-checked:block" />
           <label htmlFor="general-enquiry" className="body-m">
             Generl Enquiry
           </label>
         </div>
-        <div className="h-[51px] space-x-3 rounded-lg border border-grey500 px-4 py-3 md:flex-1">
+        <div
+          className={` ${radioChecked === "support-request" ? "border-green600 bg-green200" : "border-grey500"} relative flex h-[51px] items-center space-x-3 rounded-lg border px-6 py-3 md:flex-1`}
+        >
           <input
-            type="checkbox"
+            checked={radioChecked === "support-request"}
+            type="radio"
             id="support-request"
-            name="support-request"
+            name="query-type"
+            value="support-request"
+            className="radioIconStyle peer"
+            onChange={() => setRadioChecked("support-request")}
           />
-          {/* <IconRadioSelected /> */}
+          <IconRadioSelected className="pointer-events-none absolute left-3 hidden peer-checked:block" />
           <label htmlFor="support-request" className="body-m">
             Support Request
           </label>
