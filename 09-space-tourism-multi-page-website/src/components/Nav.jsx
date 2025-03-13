44cfd82e1eee45a038c869bb6e9ad09f6d78ebb2
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-13 23:07:46 +0800

    fix(nav): correct navigation layout

--- 09-space-tourism-multi-page-website/src/components/Nav.jsx
+++ 09-space-tourism-multi-page-website/src/components/Nav.jsx
@@ -9,16 +9,16 @@ const ROUTES = [
 
 export function Nav() {
   return (
-    <nav className="absolute top-0 1440:top-10 flex h-[88px] w-full items-center justify-between p-6 md:h-24 660:p-0">
+    <nav className="absolute flex h-[88px] w-full items-center justify-between p-6 text-white 660:p-0 md:h-24 1440:top-10">
       <a href="#" className="1440:relative">
-        <hr className="w-[325%] h-[1px] absolute left-[100%] top-[50%] border-white border-opacity-100 1440:block hidden" />
-        <IconLogo className="1440:mx-16 h-10 w-10 660:mx-10 md:h-12 md:w-12" />
+        <hr className="absolute left-[100%] top-[50%] hidden h-[1px] w-[325%] border-[#979797] 1440:block" />
+        <IconLogo className="h-10 w-10 660:mx-10 md:h-12 md:w-12 1440:mx-16" />
       </a>
       <a href="#" className="660:hidden">
         <IconHamburger />
       </a>
-      <div className="h-full w-full max-w-[736px] hidden 660:block">
-        <ul className="flex h-full w-full items-center justify-end space-x-12 bg-white bg-opacity-15 px-10 uppercase">
+      <div className="hidden h-full w-full max-w-[736px] 660:block">
+        <ul className="flex h-full w-full items-center justify-end space-x-12 bg-white bg-opacity-5 px-10 uppercase">
           {ROUTES.map((route, i) => {
             //自動拆分數字和文字
             const parts = route.match(/^(\d+)?\s*(.*)$/);
@@ -30,7 +30,9 @@ export function Nav() {
                 className={`title-8 flex h-full items-center ${i === 0 ? "border-b-[3px] border-white" : "hover:border-b hover:border-white"}`}
               >
                 {number && (
-                  <span className="mr-3 font-bold hidden md:block">{number}</span>
+                  <span className="mr-3 hidden font-bold md:block">
+                    {number}
+                  </span>
                 )}
                 {label}
               </li>
