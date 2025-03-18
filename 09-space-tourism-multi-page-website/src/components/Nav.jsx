d18e362437131e417b6f29a675981dbab0701e9d
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-19 02:53:35 +0800

    feat(Nav): add mobile menu toggle and improve navigation UI
    - Implemented `useState` to manage mobile menu visibility (`showMenu`)
    - Added `IconClose` to allow users to close the navigation menu
    - Applied `backdrop-blur-xl` and `bg-opacity-15` for a blurred background effect
    - Improved `ROUTES` array parsing and active state detection
    - Enhanced hover effects for better UI interaction

--- 09-space-tourism-multi-page-website/src/components/Nav.jsx
+++ 09-space-tourism-multi-page-website/src/components/Nav.jsx
@@ -1,5 +1,8 @@
 import IconLogo from "src/assets/icons/logo.svg?react";
 import IconHamburger from "src/assets/icons/icon-hamburger.svg?react";
+import IconClose from "src/assets/icons/icon-close.svg?react";
+import { useState } from "react";
+
 const ROUTES = [
   { id: "", label: "00 Home" },
   { id: "destination", label: "01 Destination" },
@@ -9,7 +12,12 @@ const ROUTES = [
 import { Link, useLocation } from "react-router-dom";
 
 export function Nav() {
+  const [showMenu, setShowMenu] = useState(false);
   const location = useLocation();
+  const handleClick = () => {
+    setShowMenu((prev) => !prev);
+  };
+
   return (
     <nav className="absolute z-10 flex h-[88px] w-full max-w-[1440px] items-center justify-between p-6 text-white 660:p-0 md:h-24 1440:top-10">
       <div className="1440:relative">
@@ -17,8 +25,64 @@ export function Nav() {
         <IconLogo className="h-10 w-10 660:mx-10 md:h-12 md:w-12 1440:mx-16" />
       </div>
       <div className="660:hidden">
-        <IconHamburger />
+        <IconHamburger
+          onClick={handleClick}
+          className="cursor-pointer"
+        />
       </div>
+      {showMenu === true && (
+        <div className="fixed right-0 top-0 z-20 h-[812px] w-[67%] max-w-[736px] bg-[#080D17] bg-opacity-15 pl-8 backdrop-blur-xl 660:hidden">
+          <div className="flex justify-end space-y-12 py-8 pr-6">
+            <IconClose
+              className="h-[21px] w-6 cursor-pointer border-blue300"
+              onClick={handleClick}
+            />
+          </div>
+          <div>
+            <ul className="flex flex-col space-y-8 uppercase">
+              {ROUTES.map(({ id, label }) => {
+                //自動拆分數字和文字
+                const parts = label.match(/^(\d+)?\s*(.*)$/);
+                const number = parts[1];
+                const labelTitle = parts[2];
+
+                // 確認當前頁面是否與此項目匹配
+                const isActive =
+                  id === ""
+                    ? location.pathname === "/"
+                    : location.pathname.startsWith(`/${id}`);
+                console.log(
+                  "now",
+                  location.pathname.startsWith(`/${id}`),
+                );
+                return (
+                  <Link
+                    key={id}
+                    to={`/${id}`}
+                    className="cursor-pointer"
+                  >
+                    <li
+                      className={`title-8 flex h-full cursor-pointer items-center ${
+                        isActive
+                          ? "border-r-[3px] border-white"
+                          : "border-[rgba(255,255,255,0.5)] hover:border-r-[3px]"
+                      }`}
+                    >
+                      {number && (
+                        <span className="title-8-number">
+                          {number}
+                        </span>
+                      )}
+                      {labelTitle}
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+
       <div className="hidden h-full w-full max-w-[736px] 660:block">
         <ul className="flex h-full w-full items-center justify-end space-x-12 bg-white bg-opacity-5 px-10 uppercase">
           {ROUTES.map(({ id, label }) => {
