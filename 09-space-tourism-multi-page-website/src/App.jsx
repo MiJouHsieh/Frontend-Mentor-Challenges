ce27d27d879b7dea1cd0209b2ab0821558ea77c4
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-19 03:00:21 +0800

    feat(App): integrate React Router and setup main routes
    - Added `react-router-dom` with `BrowserRouter`, `Routes`, and `Route`
    - Implemented main page routes (`HomePage`, `DestinationPage`, `CrewPage`, `TechnologyPage`)
    - Used `Navigate` to handle default redirections (`/destination` → `/destination/moon`)
    - Integrated `Nav` component for navigation
    - Removed test heading `<h1>Hello</h1>`

--- 09-space-tourism-multi-page-website/src/App.jsx
+++ 09-space-tourism-multi-page-website/src/App.jsx
@@ -1,7 +1,41 @@
+import { Nav } from "src/components/Nav";
+import { HomePage } from "src/pages/HomePage";
+import { DestinationPage } from "src/pages/DestinationPage";
+import { CrewPage } from "src/pages/CrewPage";
+import { TechnologyPage } from "src/pages/TechnologyPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 export function App() {
   return (
-    <h1>
-      Hello
-    </h1>
-  )
+    <div className="relative flex min-w-[375px] flex-col items-center">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route
+            path="/destination"
+            element={<Navigate to="/destination/moon" />}
+          />
+          <Route
+            path="/destination/:planet"
+            element={<DestinationPage />}
+          />
+          <Route
+            path="/crew"
+            element={<Navigate to="/crew/commander" />}
+          />
+          <Route path="/crew/:position" element={<CrewPage />} />
+          <Route
+            path="/technology"
+            element={<TechnologyPage />}
+          ></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
