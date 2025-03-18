39ad72d8acfb2c596520e57b023217456c5f5da9
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-19 00:48:27 +0800

    style(CrewPage): remove outline styles

--- 09-space-tourism-multi-page-website/src/pages/CrewPage.jsx
+++ 09-space-tourism-multi-page-website/src/pages/CrewPage.jsx
@@ -10,23 +10,23 @@ export function CrewPage() {
   return (
     <section className="relative flex h-full w-full justify-center px-6 pt-[112px] md:px-10 md:pt-[136px] 1440:pt-[184px]">
       <div className="absolute left-0 top-0 -z-10 h-full w-full bg-[url('/src/assets/crew/background-crew-mobile.jpg')] bg-cover bg-center md:bg-[url('/src/assets/crew/background-crew-tablet.jpg')] 1440:bg-[url('/src/assets/crew/background-crew-desktop.jpg')]" />
-      <div className="h-full w-full space-y-6 outline outline-red-400 md:min-w-[688px] 1440:max-w-[1110px]">
-        <p className="page-title 1440:title-5 w-full outline md:text-[20px] md:leading-[24px] md:tracking-[3px]">
+      <div className="h-full w-full space-y-6 md:min-w-[688px] 1440:max-w-[1110px]">
+        <p className="page-title 1440:title-5 w-full md:text-[20px] md:leading-[24px] md:tracking-[3px]">
           <span className="font-bold opacity-25 1440:tracking-[4.72px]">
             02
           </span>
           <span className="font-normal">Meet your crew</span>
         </p>
         <div className="flex flex-col items-center space-y-8 1440:flex-row 1440:items-center 1440:space-x-8 1440:space-y-0">
-          <div className="flex h-[319px] flex-col items-center pt-10 outline outline-teal-500 md:max-w-[512px] 1440:h-[734px] 1440:max-w-none 1440:flex-1 1440:space-y-10 1440:pt-0">
+          <div className="flex h-[319px] flex-col items-center pt-10 md:max-w-[512px] 1440:h-[734px] 1440:max-w-none 1440:flex-1 1440:space-y-10 1440:pt-0">
             <div className="flex flex-col items-center 1440:flex-1 1440:justify-center">
               <p className="crew-title 1440:title-4 mb-2 md:mb-4 md:text-[24px] md:leading-[28px]">
                 {data.title}
               </p>
-              <h1 className="crew-name 1440:title-3 md:text-[40px] md:leading-[46px]">
+              <h1 className="crew-name 1440:title-3 mb-6 md:text-[40px] md:leading-[46px]">
                 {data.name}
               </h1>
-              <p className="crew-introduction md:text-[16px] 1440:mb-0 1440:text-start 1440:text-[18px]">
+              <p className="crew-introduction mb-6 md:text-[16px] 1440:mb-0 1440:text-start 1440:text-[18px]">
                 {data.introduction}
               </p>
             </div>
@@ -48,9 +48,9 @@ export function CrewPage() {
               </ul>
             </div>
           </div>
-          <div className="relative flex h-[350px] w-full items-center justify-center outline outline-teal-500 md:h-auto md:items-start 1440:h-[734px] 1440:flex-1 1440:items-center">
+          <div className="relative flex h-[350px] w-full items-center justify-center md:h-auto md:items-start 1440:h-[734px] 1440:flex-1 1440:items-center">
             <img
-              className="h-[340px] w-[271px] object-contain outline md:h-[560px] md:w-[446.7px] 1440:h-[676px] 1440:w-[540px]"
+              className="h-[340px] w-[271px] object-contain md:h-[560px] md:w-[446.7px] 1440:h-[676px] 1440:w-[540px]"
               src={data.image}
               alt="crew image"
             />
