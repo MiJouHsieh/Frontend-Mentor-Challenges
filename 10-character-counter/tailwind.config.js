a75be4e41d8db92591912ec65927e173e6b8cee0
MiJouHsieh <mijouhsieh@gmail.com>
2025-08-28 19:52:01 +0800

    feat(styles): add typography styles

--- 10-character-counter/tailwind.config.js
+++ 10-character-counter/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: "#FFFFFF",
+        neutral100: "#F2F2F7",
+        neutral200: "#E4E4EF",
+        neutral600: "#404254",
+        neutral700: "#2A2B37",
+        neutral800: "#21222C",
+        neutral900: "#12131A",
+        purple400: "#D3A0FA",
+        purple500: "#C27CF8",
+        yellow500: "#FF9F00",
+        orange500: "#FE8159",
+        orange800: "#DA3701",
+      }
+    },
   },
   plugins: [],
 }
