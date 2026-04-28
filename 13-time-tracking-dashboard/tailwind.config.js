9ddcf9a46cb3e79398cbe61f0178e7bf6ad87774
MiJouHsieh <mijouhsieh@gmail.com>
2026-04-28 18:04:32 +0800

    feat(tailwind.config): extend theme with custom colors

--- 13-time-tracking-dashboard/tailwind.config.js
+++ 13-time-tracking-dashboard/tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        white: "#FFFFFF",
+        black: "#000000",
+
+        navy950: "#0E1323",
+        navy900: "#1C204B",
+        navy800: "#33397A",
+        navy200: "#BBC0FF",
+
+        purple700: "#7335D2",
+        purple600: "#5747EA",
+        purple500: "#7078C9",
+
+        work: "#FF8B64",
+        play: "#55C2E6",
+        study: "#FF5E7D",
+        exercise: "#4BCF82",
+        social: "#7335D2",
+        selfCare: "#F1C75B",
+
+        grey200: "#D8D8D8",
+      }
+    },
   },
   plugins: [],
 }
