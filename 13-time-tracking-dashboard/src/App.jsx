9d601ef2bbad8966894a44147b54d7ab63b88f5f
MiJouHsieh <mijouhsieh@gmail.com>
2026-04-29 17:43:33 +0800

    style(layout): adjust dashboard page spacing
    Update the main page wrapper with responsive vertical padding.
    Add desktop and tablet spacing to better match the dashboard layout.

--- 13-time-tracking-dashboard/src/App.jsx
+++ 13-time-tracking-dashboard/src/App.jsx
@@ -1,12 +1,11 @@
 import { TimeDashboard } from "src/components/TimeDashboard";
 
 function App() {
-
   return (
-    <main className="min-h-screen px-6 py-[81px]">
+    <main className="lg:py=[252px] min-h-screen px-6 py-[81px] md:py-[173px]">
       <TimeDashboard />
     </main>
   );
 }
 
-export default App
+export default App;
