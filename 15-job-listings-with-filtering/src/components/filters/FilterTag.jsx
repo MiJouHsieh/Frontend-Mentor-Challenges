a91aa48d84d24597656e496ef0364b6776c089f8
MiJouHsieh <mijouhsieh@gmail.com>
2026-05-23 16:23:13 +0800

    fix(filter-tag): set button height

--- 15-job-listings-with-filtering/src/components/filters/FilterTag.jsx
+++ 15-job-listings-with-filtering/src/components/filters/FilterTag.jsx
@@ -2,7 +2,7 @@ export function FilterTag({ tag }) {
   return (
     <button
       type="button"
-      className="text-3-bold text-customGreen-400 bg-customGreen-50 hover:bg-customGreen-400 rounded-sm px-4 py-2 hover:text-white"
+      className="text-3-bold text-customGreen-400 bg-customGreen-50 hover:bg-customGreen-400 h-10 rounded-sm px-4 py-2 hover:text-white"
     >
       {tag}
     </button>
