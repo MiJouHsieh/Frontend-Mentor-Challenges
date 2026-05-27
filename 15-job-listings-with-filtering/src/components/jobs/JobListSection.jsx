7e9415d2e149c2ac08306f4503c1b10d7d96ddfc
MiJouHsieh <mijouhsieh@gmail.com>
2026-05-27 23:34:30 +0800

    feat(jobs): connect job list to filter data
    Update JobList and JobListSection to receive job data and filter click
    handlers from props.
    
    This allows the list to render filtered jobs from the parent component
    and forward tag click events to each job card.

--- 15-job-listings-with-filtering/src/components/jobs/JobListSection.jsx
+++ 15-job-listings-with-filtering/src/components/jobs/JobListSection.jsx
@@ -1,9 +1,9 @@
 import { JobList } from "src/components/jobs/JobList";
 
-export function JobListSection() {
+export function JobListSection({ jobs, onFilterClick }) {
   return (
-    <section className="1440:mt-[76px] mx-auto mb-8 mt-14 w-full">
-      <JobList />
+    <section className="1440:mt-[76px] mx-auto mb-8 mt-14 w-full h-full">
+      <JobList jobs={jobs} onFilterClick={onFilterClick} />
     </section>
   );
 }
